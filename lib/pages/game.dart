7aab43a916daed015c3543714b8ec8330f7be95d
han-yes <hangyulheaven@gmail.com>
2026-06-04 01:41:55 -0400

    Fixed the Initial Text display using FutureBuilder. Added choices and choice buttons Added text type-out and speed up options

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -8,23 +8,21 @@ import 'package:twine_parser/twine_parser.dart';
 
 
 class Game extends StatefulWidget {
-    const Game({super.key});
-    
-      @override
-      State<Game> createState() => Game_();
+  const Game({super.key});
+  
+  @override
+  State<Game> createState() => Game_();
 }
 
 class Game_ extends State<Game>{
   final parser = TwineParser();
-  Passage curPass = Passage(name: '', content: '', choices: []);
-  StreamController<Passage> passageController = StreamController<Passage>.broadcast();
-  Stream<Passage> get newPassageStream => passageController.stream;
+  late Future<Passage> futurePassage;
+  Passage? currentPassage;
 
   @override
   void initState() {
     super.initState();
-    getStartStory().then((passage) => curPass = passage);
-    passageController.add(curPass);
+    futurePassage = getStartStory();
   }
 
   @override
@@ -32,60 +30,124 @@ class Game_ extends State<Game>{
     super.dispose();
   }
 
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: StreamBuilder(
-          stream: passageController.stream, 
-          builder: (context, snapshot){
-              if(snapshot.hasData){
-                print("checkpoint");
-                curPass = snapshot.data ?? Passage(name: 'error', content: 'error', choices: []);
-              }
+        child: FutureBuilder(future: futurePassage,
 
-              return DialogueBox(speaker: 'test', text: curPass.content);
+        builder: (build, snapshot){
+          if (snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(color: const Color.fromARGB(255, 73, 163, 76),),
+            );
           }
-        )
+
+          if (snapshot.hasError){
+            return Center(
+              child: Text('Failed to load story \n ${snapshot.error}'),
+            );
+          }
+
+          return 
+          Align(
+            alignment: Alignment(0, 0.8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Align(
+                  alignment: AlignmentGeometry.xy(0.6, 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: const Color.fromARGB(255, 60, 105, 70), width: 3),
+                      borderRadius: BorderRadius.circular(5)
+                    ),
+                    child: _buildChoices(currentPassage!.choices),
+                  )
+                ),
+                const SizedBox(height: 2),
+                Align(
+                  child: DialogueBox(speaker: 'Plant Chan', text: currentPassage!.content),
+                ),
+                _speedUp()
+              ],
+            )
+          );
+        }),
       )
     );
   }
   
 
   Future<Passage> getStartStory() async{
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path;
-
     final storyHtml = await File('assets/PlantGirlTwine.html').readAsString();
 
     await parser.parseStory(storyHtml);
     final startPassage = parser.getStartPassage();
     
+    currentPassage = startPassage;
+
     return startPassage;
-  }
-
-  Future<String> get _localPath async{
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
   }
 
   Widget _buildTextBox(){
     return Container();
   }
 
-  Widget _buildChoices(){
-    return Container();
+  void updatePassage(String passageName){
+    setState(() {
+      currentPassage = parser.getPassage(passageName);
+    });
+  }
+
+  Widget _buildChoices(List<Choice> choices){
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      alignment: WrapAlignment.center,
+
+      children: 
+        choices.map((choice) {
+          return ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 73, 129, 82)
+            ),
+            onPressed: () => updatePassage(choice.targetPassage), 
+            child: Text(
+              choice.text,
+              style: TextStyle(color: Colors.white),
+            )
+          );
+        }
+      ).toList()
+    );
+  }
+
+  Widget _speedUp() {
+        return TextButton(
+          child: Text('Speed Up'),
+          onPressed: () => DialogueBox.speed = 45);
   }
 }
 
 class DialogueBox extends StatelessWidget{
   final String speaker;
   final String text;
+  String currentText = "";
+  static int speed = 70;
 
-  const DialogueBox({super.key, required this.speaker, required this.text});
+  DialogueBox({super.key, required this.speaker, required this.text});
+
+  Stream<String> _getTextStream(String fullPassage) async* {
+    for(int i=0; i < fullPassage.length; i++){
+      await Future.delayed(Duration(milliseconds: speed));
+      String currentChar = fullPassage[i];
+      yield currentChar;
+    }
+    yield ' ';
+  }
+
+  late final Stream<String> textStream = _getTextStream(text);
 
   @override
   Widget build(BuildContext context) {
@@ -96,6 +158,7 @@ class DialogueBox extends StatelessWidget{
       opacity: 1,
       child: 
       Container(
+        alignment: Alignment.bottomCenter,
         width: 1000,
         height: boxHeight,
         decoration: BoxDecoration(
@@ -123,10 +186,24 @@ class DialogueBox extends StatelessWidget{
               ),
               child: Padding(
                 padding: EdgeInsetsGeometry.only(left: 10, top: 2, bottom: 2),
-                child: Text(
-                  text, 
-                  style: TextStyle(fontSize: 25)
-                )
+                child: StreamBuilder<String>(
+                          stream: textStream,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              currentText += snapshot.requireData;
+                              return Text(
+                                currentText,
+                                style: TextStyle(fontSize: 25)
+                              );
+                            }
+                            else{
+                              return Text(
+                              'Error, Stream not found', 
+                              style: TextStyle(fontSize: 25)
+                            );
+                            }
+                          },
+                        ),
               ),
             )
           ],

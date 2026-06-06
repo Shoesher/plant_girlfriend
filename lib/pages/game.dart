b1c093fea631f4b60663f652c34ef7dc7562cdbb
han-yes <hangyulheaven@gmail.com>
2026-06-06 14:33:41 -0400

    Added Background image support and image precaching for future choices

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -16,7 +16,11 @@ class Game_ extends State<Game> {
   final parser = TwineParser();
   late Future<Passage> futurePassage;
   Passage? currentPassage;
+  File? curSprite;
+  File? curBackground;
   int _speed = 50;
+
+  final ImageCache imageCache = PaintingBinding.instance.imageCache;
 
   @override
   void initState() {
@@ -51,15 +55,37 @@ class Game_ extends State<Game> {
               );
             }
 
-            List parseList = parseImage(currentPassage!.content);
-            String imgUrl = parseList[0];
-            String content = parseList[1];
+            List parseList = parseAssets(currentPassage!.content);
+            String spriteName = parseList[0];
+            String backgroundName = parseList[1];
+            String content = parseList[2];
+            
+            for (Choice choice in currentPassage!.choices){
+              Passage passage = parser.getPassage(choice.targetPassage)!;
+              List<String> list = parseAssets(passage.content);
+              String sprite = list[0];
+              String background = list[1];
+              if (sprite.isNotEmpty){
+                precacheImage(FileImage(File('assets/PlantGirl_Images/$sprite')), context);
+              }
 
+              if (background.isNotEmpty){
+                precacheImage(FileImage(File('assets/PlantGirl_Images/$background')), context);
+              }
+            }
+            
+            curSprite = spriteName.isNotEmpty ? File('assets/PlantGirl_Images/$spriteName') : curSprite;
+            curBackground = backgroundName.isNotEmpty ? File('assets/Background_Images/$backgroundName') : curBackground;
+            
             return Stack(
               alignment: AlignmentGeometry.center,
               children: [
-                Image.network(
-                  imgUrl,
+                Image.file(
+                  curBackground ?? File('assets/Background_Images/'),
+                  errorBuilder: (context, error, stackTrace) => Container(),
+                ),
+                Image.file(
+                  curSprite ?? File('assets/PlantGirl_Images/'),
                   errorBuilder: (context, error, stackTrace) {
                     return Container();
                   },
@@ -106,18 +132,21 @@ class Game_ extends State<Game> {
 
   /// First value returns the image url.
   /// Second value returns the rest of the content.
-  List<String> parseImage(String content) {
+  List<String> parseAssets(String content) {
     // Extract text inside <>
-    RegExp tagRegex = RegExp(r'<(.*?)>');
-    Match? match = tagRegex.firstMatch(content);
-    String? tagContent = match?.group(
-      1,
-    ); // Gets the content on the inside of the <>
+    RegExp spriteRegex = RegExp(r'<(.*?)>');
+    Match? spriteMatch = spriteRegex.firstMatch(content);
+    String? spriteContent = spriteMatch?.group(1); // Gets the content on the inside of the <>
+    
+    RegExp bckgrndRegex = RegExp(r'\/(.*?)\/');
+    Match? bckgrndMatch = bckgrndRegex.firstMatch(content);
+    String? bckgrndContent = bckgrndMatch?.group(1); // Gets the content on the inside of the <>
 
     // Remove the <> and its contents from the string
-    String rest = content.replaceAll(RegExp(r'\s*<.*?>\s'), '').trim();
+    String rest = content.replaceAll(RegExp(r'<.*?>'), '');
+    rest = rest.replaceAll(RegExp(r'\/.*?\/'), '').trim();
 
-    List<String> result = [tagContent ?? '', rest];
+    List<String> result = [spriteContent ?? '', bckgrndContent ?? '', rest];
 
     return result;
   }
@@ -140,6 +169,7 @@ class Game_ extends State<Game> {
   void updatePassage(String passageName) {
     setState(() {
       currentPassage = parser.getPassage(passageName);
+      imageCache.clear();
     });
   }
 
@@ -166,7 +196,15 @@ class Game_ extends State<Game> {
 
   Widget _speedUp() {
     return TextButton(
-      child: Text('Speed Up'),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 164, 112, 185)
+      ),
+      child: Text(
+        'Speed Up',
+        style: TextStyle(
+          color: Color.fromARGB(192, 255, 255, 255)
+        ),
+      ),
       onPressed: () => setState(() => _speed = 20),
     );
   }
